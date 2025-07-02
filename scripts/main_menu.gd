4c8f06c7fc71405d620d789714547336fcf066bf
Alvaro <alvaro.francisco.gil@gmail.com>
2025-07-02 14:37:22 +0200

    add game manager

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,6 +5,13 @@ func _ready():
 	$VBoxContainer/ParejasButton.pressed.connect(_on_parejas_button_pressed)
 	$VBoxContainer/CartapumButton.pressed.connect(_on_cartapum_button_pressed)
 	$VBoxContainer/QuitButton.pressed.connect(_on_quit_button_pressed)
+	
+	# Add cross-minigame hover and click effects to all buttons
+	if GameManager:
+		GameManager.add_hover_effect($VBoxContainer/SilabasButton)
+		GameManager.add_hover_effect($VBoxContainer/ParejasButton)
+		GameManager.add_hover_effect($VBoxContainer/CartapumButton)
+		GameManager.add_hover_effect($VBoxContainer/QuitButton)
 
 func _on_silabas_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/minigames/silabas/game.tscn")
